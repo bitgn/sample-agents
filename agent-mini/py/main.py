6964b8257f2ef9d8771829ca1ead2591b05259ac
Rinat Abdullin <rinat@abdullin.com>
2026-03-16 10:56:24 +0100

    Cleanup

--- agent-mini/py/main.py
+++ agent-mini/py/main.py
@@ -1,11 +1,9 @@
 from harness.agent_mini.agent_mini_connect import AgentMiniHarnessClientSync
-from platform.benchmark.benchmark_connect import BenchmarkServiceClientSync
 
 
 def main():
     print("Hello from python!")
 
-    client = BenchmarkServiceClientSync(address="http://127.0.0.1:8080")
 
     harness = AgentMiniHarnessClientSync(address="http://127.0.0.1:8080")
 
